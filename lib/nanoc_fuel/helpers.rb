0da6ee07a35f6ff5d1cb8962fc91f30a0ec90ace
robertjwhitney <robertj.whitney@gmail.com>
2011-04-29 19:32:40 -0400

    too much copypasta

--- lib/nanoc_fuel/helpers.rb
+++ lib/nanoc_fuel/helpers.rb
@@ -3,7 +3,7 @@
 module NanocFuel
   module Helpers
     autoload "Facebook", "nanoc_fuel/helpers/facebook"
-    autoload "Facebook", "nanoc_fuel/helpers/google_analytics"
+    autoload "Google", "nanoc_fuel/helpers/google_analytics"
     autoload "RuleTemplates", "nanoc_fuel/helpers/rule_templates"
   end
 end
