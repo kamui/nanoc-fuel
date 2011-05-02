61a73b63d75afe862128c61a8185b00b6ce6810d
Jack Chu <kamuigt@gmail.com>
2011-05-02 15:16:22 -0400

    clean up helpers, redunadant methods

--- lib/nanoc_fuel/helpers/facebook.rb
+++ lib/nanoc_fuel/helpers/facebook.rb
@@ -5,19 +5,11 @@ require 'haml'
 module NanocFuel::Helpers
    module Facebook
      def fb_init(app_id)
-       fb_init_haml(app_id)
-     end
-
-     def fb_init_haml(app_id)
        template = File.read(File.expand_path("../templates/facebook/fb_init.haml", __FILE__))
        Haml::Engine.new(template).render(Object.new, :app_id => app_id)
      end
      
      def fb_comments(url, num_posts, width)
-       fb_comments_haml(url, num_posts, width)
-     end
-     
-     def fb_comments_haml(url, num_posts, width)
        template = File.read(File.expand_path("../templates/facebook/fb_comments.haml", __FILE__))
        Haml::Engine.new(template).render(Object.new, :url => url, :num_posts => num_posts, :width => width)
      end
--- lib/nanoc_fuel/helpers/google.rb
+++ lib/nanoc_fuel/helpers/google.rb
@@ -5,10 +5,6 @@ require 'haml'
 module NanocFuel::Helpers
    module Google
     def ga_tracking(ua_id)
-      ga_tracking_code_haml(ua_id)
-    end
-
-    def ga_tracking_code_haml(ua_id)
       template = File.read(File.expand_path("../templates/google/ga_tracking_code.haml", __FILE__))
       Haml::Engine.new(template).render(Object.new, :ua_id => ua_id)
     end
