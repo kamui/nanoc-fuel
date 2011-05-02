ff0ca8d483698c3e0414839d79e6a4996f651545
robertjwhitney <robertj.whitney@gmail.com>
2011-05-02 19:28:01 -0400

    full passing tests for facebook

--- test/helpers/facebook_test.rb
+++ test/helpers/facebook_test.rb
@@ -30,4 +30,19 @@ class NanocFuel::Helpers::FacebookTest < MiniTest::Unit::TestCase
     }
     assert_equal compact(html), compact(fb_init('12345'))
   end
+  
+  def test_fb_comments
+    html = %q{
+      <fb:comments href='http://example.com' num_posts='10' width='520'></fb:comments>
+    }
+    assert_equal compact(html), compact(fb_comments('http://example.com', '10', '520'))
+  end
+  
+  def test_fb_like_btn
+    html = %q{
+      <iframe allowTransparency='true' frameborder='0' scrolling='no' src='http://www.facebook.com/plugins/like.php?href&amp;amp;send=true&amp;amp;layout=standard&amp;amp;width=450&amp;amp;show_faces=true&amp;amp;action=like&amp;amp;colorscheme=light&amp;amp;font&amp;amp;height=80' style='border:none; overflow:hidden; width:450px; height:80px;'></iframe>
+    }
+    assert_equal compact(html), compact(fb_like_btn("standard", "450", "true", "true"))
+  end
+  
 end
