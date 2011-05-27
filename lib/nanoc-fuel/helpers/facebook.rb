44e279c58c5db53322ec16566a663e5c990c7340
Winter Lee <winterl@bigfuel.com>
2011-05-27 14:15:51 -0400

    added fb_meta_admin helper

--- lib/nanoc-fuel/helpers/facebook.rb
+++ lib/nanoc-fuel/helpers/facebook.rb
@@ -8,16 +8,20 @@ module NanocFuel::Helpers
        template = File.read(File.expand_path("../templates/facebook/fb_init.haml", __FILE__))
        Haml::Engine.new(template).render(Object.new, :app_id => app_id)
      end
-     
+
      def fb_comments(url, num_posts, width)
        template = File.read(File.expand_path("../templates/facebook/fb_comments.haml", __FILE__))
        Haml::Engine.new(template).render(Object.new, :url => url, :num_posts => num_posts, :width => width)
      end
-     
+
      def fb_like_btn(layout, width, show_faces, send)
        template = File.read(File.expand_path("../templates/facebook/fb_like_btn.haml", __FILE__))
        Haml::Engine.new(template).render(Object.new, :layout => layout, :width => width, :show_faces => show_faces, :send => send)
      end
-     
+
+     def fb_meta_admin(meta_admin)
+       template = File.read(File.expand_path("../templates/facebook/fb_meta_admin.haml", __FILE__))
+       Haml::Engine.new(template).render(Object.new, :meta_admin => meta_admin)
+     end
    end
 end
--- test/helpers/facebook_test.rb
+++ test/helpers/facebook_test.rb
@@ -4,7 +4,6 @@ class NanocFuel::Helpers::FacebookTest < MiniTest::Unit::TestCase
   include NanocFuel::Helpers::Facebook
 
   def test_fb_init
-    
     html = %q{
 <div id='fb-root'></div>
 <script type='text/javascript'>
@@ -31,19 +30,25 @@ class NanocFuel::Helpers::FacebookTest < MiniTest::Unit::TestCase
     }
     assert_equal compact(html), compact(fb_init('12345'))
   end
-  
+
   def test_fb_comments
     html = %q{
       <fb:comments href='http://example.com' num_posts='10' width='520'></fb:comments>
     }
     assert_equal compact(html), compact(fb_comments('http://example.com', '10', '520'))
   end
-  
+
   def test_fb_like_btn
     html = %q{
       <iframe allowTransparency='true' frameborder='0' scrolling='no' src='//www.facebook.com/plugins/like.php?href&amp;amp;send=true&amp;amp;layout=standard&amp;amp;width=450&amp;amp;show_faces=true&amp;amp;action=like&amp;amp;colorscheme=light&amp;amp;font&amp;amp;height=80' style='border:none; overflow:hidden; width:450px; height:80px;'></iframe>
     }
     assert_equal compact(html), compact(fb_like_btn("standard", "450", "true", "true"))
   end
-  
+
+  def test_fb_meta_admin
+    html = %q{
+      <meta content='210427235657242' property='fb:admins' />
+    }
+    assert_equal compact(html), compact(fb_meta_admin("210427235657242"))
+  end
 end
