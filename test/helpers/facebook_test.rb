f67e5498a98aa08fd9d5f785fde382300af3037d
Jack Chu <kamuigt@gmail.com>
2012-02-15 15:57:35 -0500

    Fix fb init test

--- test/helpers/facebook_test.rb
+++ test/helpers/facebook_test.rb
@@ -9,22 +9,25 @@ class NanocFuel::Helpers::FacebookTest < MiniTest::Unit::TestCase
 <script type='text/javascript'>
   //<![CDATA[
     window.fbAsyncInit = function() {
-      FB.init({appId: '12345', status: true, cookie: true, xfbml: true});
+      FB.init({
+        appId: '12345',
+        status: true,
+        cookie: true,
+        xfbml: true
+      });
       fbInitHook();
-      FB.Canvas.setSize();
     };
-
-    function fbInitHook() {}
 
     function sizeChangeCallback() {
       FB.Canvas.setSize();
     }
 
-    (function() {
-      var e = document.createElement('script'); e.async = true;
-      e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-      document.getElementById('fb-root').appendChild(e);
-    }());
+    (function(d){
+       var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+       js = d.createElement('script'); js.id = id; js.async = true;
+       js.src = "//connect.facebook.net/en_US/all.js";
+       d.getElementsByTagName('head')[0].appendChild(js);
+     }(document));
   //]]>
 </script>
     }
